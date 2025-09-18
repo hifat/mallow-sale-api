b0b5705d47a42e6d04b2dd5e8540e9db2de18dd0
hifat <logmanxsa@gmail.com>
2025-09-19 03:51:16 +0700

    test(inventory): add Find func unit test

--- internal/inventory/service/service_test.go
+++ internal/inventory/service/service_test.go
@@ -11,6 +11,7 @@ import (
 	mockInventoryRepository "github.com/hifat/mallow-sale-api/internal/inventory/repository/mock"
 	usageUnitModule "github.com/hifat/mallow-sale-api/internal/usageUnit"
 	mockUsageUnitRepository "github.com/hifat/mallow-sale-api/internal/usageUnit/repository/mock"
+	utilsModule "github.com/hifat/mallow-sale-api/internal/utils"
 	"github.com/hifat/mallow-sale-api/pkg/define"
 	"github.com/hifat/mallow-sale-api/pkg/handling"
 	mockLogger "github.com/hifat/mallow-sale-api/pkg/logger/mock"
@@ -261,5 +262,119 @@ func (s *testInventoryServiceSuite) TestInventoryService_Create() {
 		s.Require().Nil(err)
 
 		s.Require().Equal(&mockReq, res.Item)
+	})
+}
+
+func (s *testInventoryServiceSuite) TestInventoryService_Find() {
+	s.T().Parallel()
+
+	s.Run("failed - count inventories", func() {
+		ctx := context.Background()
+
+		mockErr := errors.New("mock err")
+		s.mockInventoryRepo.EXPECT().
+			Count(ctx).
+			Return(int64(0), mockErr).
+			Times(1)
+
+		s.mockLogger.EXPECT().
+			Error(mockErr).
+			Times(1)
+
+		mockQuery := utilsModule.QueryReq{}
+
+		res, err := s.underTest.Find(ctx, &mockQuery)
+		s.Require().Nil(res)
+		s.Require().NotNil(err)
+		s.Require().IsType(handling.ErrorResponse{}, err)
+
+		resErr := err.(handling.ErrorResponse)
+
+		s.Require().Equal(define.CodeInternalServerError, resErr.Code)
+		s.Require().Equal(define.MsgInternalServerError, resErr.Message)
+		s.Require().Equal(http.StatusInternalServerError, resErr.Status)
+	})
+
+	s.Run("failed - find inventories", func() {
+		ctx := context.Background()
+
+		s.mockInventoryRepo.EXPECT().
+			Count(ctx).
+			Return(int64(5), nil).
+			Times(1)
+
+		mockQuery := utilsModule.QueryReq{}
+
+		mockErr := errors.New("mock err")
+		s.mockInventoryRepo.EXPECT().
+			Find(ctx, &mockQuery).
+			Return(nil, mockErr).
+			Times(1)
+
+		s.mockLogger.EXPECT().
+			Error(mockErr).
+			Times(1)
+
+		res, err := s.underTest.Find(ctx, &mockQuery)
+		s.Require().Nil(res)
+		s.Require().NotNil(err)
+		s.Require().IsType(handling.ErrorResponse{}, err)
+
+		resErr := err.(handling.ErrorResponse)
+
+		s.Require().Equal(define.CodeInternalServerError, resErr.Code)
+		s.Require().Equal(define.MsgInternalServerError, resErr.Message)
+		s.Require().Equal(http.StatusInternalServerError, resErr.Status)
+	})
+
+	s.Run("succeed - return empty slice", func() {
+		ctx := context.Background()
+
+		s.mockInventoryRepo.EXPECT().
+			Count(ctx).
+			Return(int64(5), nil).
+			Times(1)
+
+		mockQuery := utilsModule.QueryReq{}
+
+		s.mockInventoryRepo.EXPECT().
+			Find(ctx, &mockQuery).
+			Return(nil, nil).
+			Times(1)
+
+		res, err := s.underTest.Find(ctx, &mockQuery)
+		s.Require().Nil(err)
+		s.Require().NotNil(res)
+		s.Require().IsType([]inventoryModule.Response{}, res.Items)
+		s.Require().NotNil(res.Meta)
+
+	})
+
+	s.Run("succeed - find inventories", func() {
+		ctx := context.Background()
+		var total int64 = 3
+
+		s.mockInventoryRepo.EXPECT().
+			Count(ctx).
+			Return(total, nil).
+			Times(1)
+
+		mockQuery := utilsModule.QueryReq{}
+
+		mockInventories := make([]inventoryModule.Response, total)
+		gofakeit.Slice(&mockInventories)
+
+		s.mockInventoryRepo.EXPECT().
+			Find(ctx, &mockQuery).
+			Return(mockInventories, nil).
+			Times(1)
+
+		res, err := s.underTest.Find(ctx, &mockQuery)
+		s.Require().Nil(err)
+		s.Require().NotNil(res)
+		s.Require().IsType([]inventoryModule.Response{}, res.Items)
+		s.Require().Equal(total, int64(len(res.Items)))
+		s.Require().NotNil(res.Meta)
+		s.Require().Equal(total, res.Meta.Total)
 	})
 }
