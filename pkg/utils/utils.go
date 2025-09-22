61cbe9d8fe92717ba35bf36d416371201c7d54ad
hifat <logmanxsa@gmail.com>
2025-09-22 13:23:36 +0700

    utils: add get code, id func

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
-import "math"
+import (
+	"math"
+)
 
 // It returns an empty slice if the input is nil.
 func MustToSlice[T any](v []T) []T {
@@ -13,4 +15,30 @@ func MustToSlice[T any](v []T) []T {
 
 func RoundToDecimals(value float64, decimals int) float64 {
 	return math.Round(value*math.Pow(10, float64(decimals))) / math.Pow(10, float64(decimals))
+}
+
+type IID interface {
+	GetID() string
+}
+
+func GetIDs[T IID](items []T) []string {
+	ids := make([]string, 0, len(items))
+	for _, v := range items {
+		ids = append(ids, v.GetID())
+	}
+
+	return ids
+}
+
+type ICode interface {
+	GetCode() string
+}
+
+func GetCodes[T ICode](items []T) []string {
+	codes := make([]string, 0, len(items))
+	for _, v := range items {
+		codes = append(codes, v.GetCode())
+	}
+
+	return codes
 }
